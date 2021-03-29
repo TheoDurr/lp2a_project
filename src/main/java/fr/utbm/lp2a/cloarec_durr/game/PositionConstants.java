a782f71a9cbdd0aa63143395f50f416a777f368f
Florian CLOAREC <39800745+Flo3171@users.noreply.github.com>
2021-03-29 11:28:36 +0200

    implement Position class

---
+++ src/main/java/fr/utbm/lp2a/cloarec_durr/game/PositionConstants.java
@@ -0,0 +1,11 @@
+package fr.utbm.lp2a.cloarec_durr.game;
+
+public final class PositionConstants {
+    public static final int START = 0;
+    public static final int DELTA = 5*2+3;
+    public static final int ONE_TURN = DELTA*4;
+    public static final int HOME = DELTA * 4 + 5;//57
+    public static final int ARROW = ONE_TURN -1;//51
+    public static final int STAR = 5+4;//9
+
+}
