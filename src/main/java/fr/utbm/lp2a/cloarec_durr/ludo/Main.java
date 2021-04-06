6bbcde09ba435955b9ded1624fac36a0afa84a7b
Florian CLOAREC <39800745+Flo3171@users.noreply.github.com>
2021-04-06 16:05:29 +0200

    start work about the graphical interface

---
+++ src/main/java/fr/utbm/lp2a/cloarec_durr/ludo/Main.java
@@ -0,0 +1,13 @@
+package fr.utbm.lp2a.cloarec_durr.ludo;
+
+import fr.utbm.lp2a.cloarec_durr.ludo.gui.MySwing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new MySwing();
+
+    }
+}
--- src/main/java/fr/utbm/lp2a/cloarec_durr/ludo/game/items/coordinates/PositionConstants.java
+++ src/main/java/fr/utbm/lp2a/cloarec_durr/ludo/game/items/coordinates/PositionConstants.java
@@ -5,7 +5,7 @@ public final class PositionConstants {
     public static final int START = 0;
     public static final int DELTA = 5*2+3;
     public static final int ONE_TURN = DELTA*4;
-    public static final int HOME = DELTA * 4 + 4;//57
+    public static final int HOME = DELTA * 4 + 4;//56
     public static final int ARROW = ONE_TURN -2;//50
     public static final int STAR = 5+4;//9
 
